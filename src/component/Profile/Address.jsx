98939381c73bae6328a1ddbbf78611b64ca68d9b
RADY <omaymaradyy@gmail.com>
2024-08-06 23:18:17 +0100

    ma partie

---
+++ src/component/Profile/Address.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Address =() =>{
+return(
+
+    <div>
+
+    </div>
+);
+
+}
--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ProfileNavigation } from './ProfileNavigation'
+import { UserProfile} from './UserProfile'
+import {Routes ,Route  } from 'react-router-dom'
+import { Orders } from './Orders';
+import { Address } from './Address';
+import { Favorites } from './Favorites';
+import { Events } from './Events';
 const Profile = () => {
+    const [openSideBar, setOpenSideBar]=useState(false);
     return (
         <div className= 'lg:flex justify-between'>
             <div className='stiky h-[80vh] lg:w-[20%]'>
-               <ProfileNavigation/>
+               <ProfileNavigation open={openSideBar}/>
             </div>
             <div className='lg:w-[80%]'>
+ 
+    <Routes>
+
+        <Route path='/' element={<UserProfile/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+        <Route path='/address' element={<Address/>}/>
+        <Route path='/favorites' element={<Favorites/>}> </Route>
+        <Route path='/events' element={< Events/>} > </Route>
+
+        
+
+    </Routes>
                 
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Profile
+export default Profile;
